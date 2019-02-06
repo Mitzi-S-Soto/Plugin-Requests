6ac852fca7f9ad99cae99fdc2ef2990920931bfc
unknown <batemanvo@gmail.com>
2019-02-06 15:15:58 -0800

    Handling for multiple attackTypes, as well as moving the execution when not supporting YEP_BattleEngineCore to action start instead of invocation.

--- js/plugins/FirstStrike.js
+++ js/plugins/FirstStrike.js
@@ -29,20 +29,75 @@
     var parameters = PluginManager.parameters('FirstStrike');
     var firstStrikeMessage = parameters.firstStrikeMessage;
 
-    FirstStrike.shouldPerformStrike = function(dataBattler) {
-        var strikeType = dataBattler.meta.firstStrike;
-        if (!strikeType) { return false; }
-        if (strikeType.match(/all/i)) { return true; }
+    function reduceTypes(types, item) {
+        return types.concat((item.meta.attackTypes || '').split(','));
+    }
 
+    FirstStrike.getAttackTypes = function(subject) {
+        var itemTypes = (BattleManager._action.item().meta.attackTypes || '').split(',');
 
+        if (subject instanceof Game_Enemy) {
+            var enemyTypes = ($dataEnemies[subject._enemyId].meta.attackTypes || '').split(',');
+            return itemTypes.concat(enemyTypes);
+        }
+        if (subject instanceof Game_Actor) {
+            var actorTypes = ($dataActors[subject._actorId].meta.attackTypes || '').split(',');
+            var classTypes = (subject.currentClass().meta.attackTypes || '').split(',');
+            var weaponTypes = subject.weapons().reduce(reduceTypes, []);
+            var armorTypes = subject.armors().reduce(reduceTypes, []);
+            var stateTypes = subject.states().reduce(reduceTypes, []);
+            return itemTypes.concat(actorTypes).concat(classTypes).concat(weaponTypes).concat(armorTypes).concat(stateTypes);
+        }
     };
 
     Game_Battler.prototype.confirmStrikeSkill = function() {
-        var skillId = parseInt(this._strikeData.split(',')[1].trim());
+        var strikeData = this._strikeData.split(',');
+        var skillId = parseInt(strikeData[strikeData.length - 1].trim());
         var skill = $dataSkills[skillId];
         if ((skill.mpCost > this.mp) || (skill.tpCost > this.tp)) { return this.attackSkillId(); }
 
         return skillId;
+    };
+
+    Game_Battler.prototype.hasFirstStrike = function(type) {
+        if (!type) { return false; }
+
+        if (type && !(type instanceof Array)) {
+            this._strikeData = type;
+            return true;
+        }
+
+        var strikeType = type.find(function(item) {
+            return item.meta.firstStrike;
+        });
+        if (strikeType) {
+            this._strikeData = strikeType.meta.firstStrike;
+            return true;
+        }
+
+        return false;
+    };
+
+    Game_Battler.prototype.hasStrikeType = function(attackTypes) {
+        return !!this._strikeData.split(',').find(function(strikeType) {
+            return (strikeType.match(/all/i)) || (attackTypes.find(function(attackType) {
+                return attackType.match(new RegExp(strikeType, 'i'));
+            }));
+        });
+    };
+
+    Game_Enemy.prototype.shouldPerformStrike = function(subject) {
+        var attackTypes = FirstStrike.getAttackTypes(subject);
+        if (!attackTypes.length) { return false; }
+
+        if (this.hasFirstStrike(this.states())) {
+            return this.hasStrikeType(attackTypes);
+        }
+        if (this.hasFirstStrike($dataEnemies[this._enemyId].meta.firstStrike)) {
+            return this.hasStrikeType(attackTypes);
+        }
+
+        return false;
     };
 
     Game_Enemy.prototype.firstStrikeSkillId = function() {
@@ -51,29 +106,36 @@
         return this.confirmStrikeSkill();
     };
 
-    Game_Actor.prototype.findStrikeType = function(type) {
-        if (type && !(type instanceof Array)) {
-            this._strikeData = type;
-            return true;
-        }
+    Game_Actor.prototype.shouldPerformStrike = function(subject) {
+        var attackTypes = FirstStrike.getAttackTypes(subject);
+        if (!attackTypes.length) { return false; }
 
-        var equipStrike = type.find(function(equip) {
-            return equip.meta.firstStrike;
-        });
-        if (equipStrike) {
-            this._strikeData = equipStrike.meta.firstStrike;
-            return true;
+        if (this.hasFirstStrike(this.weapons())) {
+            return this.hasStrikeType(attackTypes);
+        }
+        if (this.hasFirstStrike(this.armors())) {
+            return this.hasStrikeType(attackTypes);
+        }
+        if (this.hasFirstStrike(this.states())) {
+            return this.hasStrikeType(attackTypes);
+        }
+        if (this.hasFirstStrike($dataActors[this._actorId].meta.firstStrike)) {
+            return this.hasStrikeType(attackTypes);
+        }
+        if (this.hasFirstStrike(this.currentClass().meta.firstStrike)) {
+            return this.hasStrikeType(attackTypes);
         }
 
         return false;
     };
 
     Game_Actor.prototype.firstStrikeSkillId = function() {
-        if (this.findStrikeType($dataActors[this._actorId].meta.firstStrike)) { return this.confirmStrikeSkill(); }
-        if (this.findStrikeType(this.currentClass().meta.firstStrike)) { return this.confirmStrikeSkill(); }
-        if (this.findStrikeType(this.weapons())) { return this.confirmStrikeSkill(); }
+        if (this.hasFirstStrike(this.weapons())) { return this.confirmStrikeSkill(); }
+        if (this.hasFirstStrike(this.armors())) { return this.confirmStrikeSkill(); }
+        if (this.hasFirstStrike(this.states())) { return this.confirmStrikeSkill(); }
+        if (this.hasFirstStrike($dataActors[this._actorId].meta.firstStrike)) { return this.confirmStrikeSkill(); }
 
-        this.findStrikeType(this.armors());
+        this.hasFirstStrike(this.currentClass().meta.firstStrike);
         return this.confirmStrikeSkill();
     };
 
@@ -86,15 +148,43 @@
         BattleManager._logWindow.displayFirstStrike(subject, target);
     };
 
+    FirstStrike.startBattleAction = BattleManager.startAction;
+    BattleManager.startAction = function() {
+        var subject = this._subject;
+        var action = subject.currentAction();
+        BattleManager._action = action;
+        FirstStrike._action = action;
+        if (!module.Imported || !module.Imported.YEP_BattleEngineCore) {
+            var targets = action.makeTargets();
+            this._phase = 'action';
+            this._targets = targets;
+            FirstStrike._targets = targets.clone();
+            subject.useItem(action.item());
+            this._action.applyGlobal();
+            this.refreshStatus();
+            this._logWindow.displayAction(subject, action.item());
+        } else {
+            if (!subject) return this.endAction();
+            if (!this._action) return this.endAction();
+            if (!this._action.item()) return this.endAction();
+            var targets = action.makeTargets();
+            FirstStrike._targets = targets.clone();
+            this.setTargets(targets);
+            this._allTargets = targets.slice();
+            this._individualTargets = targets.slice();
+            this._phase = 'phaseChange';
+            this._phaseSteps = ['setup', 'whole', 'target', 'follow', 'finish'];
+            this._returnPhase = '';
+            this._actionList = [];
+            subject.useItem(this._action.item());
+            this._action.applyGlobal();
+            this._logWindow.startAction(this._subject, this._action, this._targets);
+        }
+    };
+
     FirstStrike.invokeAction = BattleManager.invokeAction;
     BattleManager.invokeAction = function(subject, target) {
-        var subjectHasFirstStrike = FirstStrike.shouldPerformStrike(
-            (subject instanceof Game_Actor ? $dataActors[subject._actorId] : $dataEnemies[subject._enemyId])
-        );
-        var targetHasFirstStrike = FirstStrike.shouldPerformStrike(
-            (target instanceof Game_Actor ? $dataActors[target._actorId] : $dataEnemies[target._enemyId])
-        );
-        if (!subjectHasFirstStrike && targetHasFirstStrike) {
+        if (target.shouldPerformStrike(subject)) {
             BattleManager.invokeFirstStrike(subject, target);
             if (subject.hp <= 0) { return };
         }
@@ -102,21 +192,16 @@
         BattleManager._logWindow.push('performInvocation', subject, target);
     };
 
-    FirstStrike.updateWaitMode = Window_BattleLog.prototype.updateWaitMode;
-    Window_BattleLog.prototype.updateWaitMode = function() {
-        if (this._spriteset.isAnimationPlaying() && this._methods[0] && this._methods[0].name !== 'addText') { return true; }
-
-        FirstStrike.updateWaitMode.call(this);
-    };
-
     Window_BattleLog.prototype.displayFirstStrike = function(subject, target) {
         this.push('addText', target.name() + ' ' + firstStrikeMessage);
         if (!module.Imported || !module.Imported.YEP_BattleEngineCore) {
             this.startAction(target, target._firstStrikeAction, [subject]);
+            this.push('updateStatus');
             this.displayActionResults(target, subject);
             this.endAction(target);
         } else {
             target.performAction(target._firstStrikeAction);
+            this.push('updateStatus');
             this.displayActionResults(target, subject);
             this.showAnimation(target, [subject], target._firstStrikeAction.item().animationId);
             this.waitForAnimation();
@@ -133,6 +218,27 @@
             subject.performCollapse();
             return;
         }
+        if (!module.Imported || !module.Imported.YEP_BattleEngineCore) {
+            this.push('performActionStart', subject, FirstStrike._action);
+            this.push('waitForMovement');
+            this.push('performAction', subject, FirstStrike._action);
+            this.push('showAnimation', subject, FirstStrike._targets.clone(), FirstStrike._action.item().animationId);
+        }
+        this.push('invokeAction', subject, target);
+    };
+
+    Window_BattleLog.prototype.invokeAction = function(subject, target) {
         FirstStrike.invokeAction.call(BattleManager, subject, target);
+    };
+
+    Window_BattleLog.prototype.updateStatus = function() {
+        BattleManager._statusWindow.refresh();
+    };
+
+    FirstStrike.updateWaitMode = Window_BattleLog.prototype.updateWaitMode;
+    Window_BattleLog.prototype.updateWaitMode = function() {
+        if (this._spriteset.isAnimationPlaying() && this._methods[0] && this._methods[0].name !== 'addText') { return true; }
+
+        FirstStrike.updateWaitMode.call(this);
     };
 })(window);
