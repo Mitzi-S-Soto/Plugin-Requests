dd1ccb79c71832784d50ef57fc641d0b53fca57d
unknown <batemanvo@gmail.com>
2019-02-06 15:57:16 -0800

    Help file.

--- js/plugins/FirstStrike.js
+++ js/plugins/FirstStrike.js
@@ -3,9 +3,69 @@
 * armors, or to indicate that an Actor, Enemy, or Class has first strike.
 * @author Zevia
 *
-* @help First strike is a counterattack that happens before the original
+* @help First Strike is a counterattack that happens before the original
 * subject's attack is performed. If the subject is still alive after
-* taking first strike damage, then their attack continues as normal.
+* taking First Strike damage, then their attack continues as normal.
+* Regular counterattacks will still occur as normal - First Strike is
+* its own responsive attack.
+*
+* First Strikes will occur when a target has a firstStrike against a
+* specified attackType. AttackTypes can be set on Actors, Enemies,
+* Classes, States, Weapons, Armors, and Items by using the <attackTypes>
+* tag. Comma-separate each type. For example:
+*
+* <attackTypes: piercing, charging>
+*
+* AttackTypes are combined across all categories, so if a Battler has
+* a weapon with a piercing attackType, a state with a magical
+* attackType, and armor with a bashing attackType, then the Battler
+* has the attackTypes piercing, magical, and bashing.
+*
+* FirstStrike can be indicated with a <firstStrike> tag, followed by
+* a comma-separated list of every type that Battler, Class, Weapon,
+* Armor, or State will have First Strike against, as well as the
+* skill ID of the skill that should be used to strike back. Skill
+* IDs should always be the last item in the list. For example:
+*
+* <firstStrike: charging, 1>
+*
+* That would indicate that if the target is attacked by a subject
+* with an attackType of charging, they will have First Strike using
+* skill 1 (Attack by default).
+*
+* First Strike is not cumulative and instead takes a priority approach.
+* Battlers will use the first First Strike category and skill ID
+* according to the following list:
+*
+* Enemies
+* 1. States
+* 2. Enemy Notetag
+*
+* Actors
+* 1. Weapons
+* 2. Armors
+* 3. States
+* 4. Actor Notetag
+* 5. Class Notetag
+*
+* For example, if an Enemy has attackTypes piercing and charging,
+* and an Actor is wearing a Weapon with <firstStrike: bashing, charging, 1>
+* and an Armor with <firstStrike: piercing, 9>, then they will perform
+* a First Strike with skill ID 1 because the Weapon is the highest
+* priority that matches one of the Enemy's attackTypes.
+*
+* If the Actor instead had a Weapon with <firstStrike: bashing, 1>
+* and an Armor with <firstStrike: charging, 9>, then they will perform
+* a First Strike with skill ID 9 because their Armor is the highest-
+* priority item that matches one of the attacker's attackTypes.
+*
+* First Strikes consume resources when using skills, so MP and TP
+* is expended when using First Strike. If a Battler cannot afford
+* to pay for the cost of their First Strike skill, they will
+* default to using their basic attack.
+*
+* This Plugin should be compatible with YEP_BattleEngineCore and
+* YEP_X_ActSeqPack2. Any other Plugins have not been tested.
 *
 * @param firstStrikeMessage
 * @text First Strike Message
@@ -102,6 +162,8 @@
 
     Game_Enemy.prototype.firstStrikeSkillId = function() {
         var dataBattler = $dataEnemies[this._enemyId];
+        if (this.hasFirstStrike(this.states())) { return this.confirmStrikeSkill(); }
+
         this._strikeData = dataBattler.meta.firstStrike;
         return this.confirmStrikeSkill();
     };
@@ -184,6 +246,7 @@
 
     FirstStrike.invokeAction = BattleManager.invokeAction;
     BattleManager.invokeAction = function(subject, target) {
+        if (subject.hp <= 0) { return; }
         if (target.shouldPerformStrike(subject)) {
             BattleManager.invokeFirstStrike(subject, target);
             if (subject.hp <= 0) { return };
